30d0218610afb074a29346cb6eb21c033fc89069
joivesca <joivesca@gmail.com>
2016-07-11 17:22:02 -0400

    1

--- uaiContacts/src/main/java/mx/com/chilitech/uaiContacts/model/Role.java
+++
@@ -1,5 +0,0 @@
-package mx.com.chilitech.uaiContacts.model;
-
-public enum Role {
-    ROLE_ADMIN, ROLE_USER
-}
--- uaiContacts/src/main/java/mx/com/chilitech/uaiContacts/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package mx.com.chilitech.uaiContacts.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import mx.com.chilitech.uaiContacts.model.User;;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-    User findByEmail(String email);
-}
