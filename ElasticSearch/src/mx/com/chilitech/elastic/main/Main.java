248d32eadf629acda719255aa5a0f9a5c7a6cb72
joivesca <joivesca@gmail.com>
2016-07-15 14:46:34 -0400

    1

--- ElasticSearch/src/mx/com/chilitech/elastic/main/Main.java
+++ ElasticSearch/src/mx/com/chilitech/elastic/main/Main.java
@@ -8,12 +8,16 @@ import java.util.Map;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.Client;
+import org.elasticsearch.node.Node;
+import static org.elasticsearch.node.NodeBuilder.nodeBuilder;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import io.searchbox.client.config.*;
 import io.searchbox.client.JestClientFactory;
 import io.searchbox.client.config.HttpClientConfig;
+import io.searchbox.client.http.JestHttpClient;
 import mx.com.chilitech.elastic.model.Book;
 import mx.com.chilitech.elastic.model.Publisher;
 
@@ -41,27 +45,42 @@ public class Main {
 			e.printStackTrace();
 		}
 		
-		//Insert
-		Client client;
+		//Iniciar elasticSearch
+		//Node node = nodeBuilder().clusterName("develop-chilitech").node();
+		Client client = null;
 		
-		IndexResponse response = client.prepareIndex(indexName, "book")
+		//Insert
+		
+		IndexResponse response = client.prepareIndex("ff", "book", "1")
 				.setSource(value).execute().actionGet();
 		
+		/*IndexResponse response = client.prepareIndex("twitter", "tweet", "1")
+		        .setSource(jsonBuilder()
+		                    .startObject()
+		                        .field("user", "kimchy")
+		                        .field("postDate", new Date())
+		                        .field("message", "trying out Elasticsearch")
+		                    .endObject()
+		                  )
+		        .get();*/
+		
 		//get
-		GetResponse getResponse = client.prepareGet(indexName, "book", response.getId())
+		GetResponse getResponse = client.prepareGet("ff", "book", response.getId())
 				.execute().actionGet();
 		String source = getResponse.getSourceAsString();
-		Book persistedBook = mapper.readValue(source, Book.class);
+		//Book persistedBook = mapper.readValue(source, Book.class);
 		
 		//Only show the result
 		Map<String, Object> sourceAsMap = getResponse.getSourceAsMap();
 		
 		
-		//Client JEST
-		JestClientFactory factory = new JestClientFactory();
-		factory.setHttpClientConfig(new HttpClientConfig.Builder(server)
-				.multithread(true).build());
 		
-		JestClient client = factory.getObject();
+		//Client JEST
+		/*HttpClientConfig clientConfig = new HttpClientConfig.Builder(server)
+				.multiThreaded(true).build();*/
+		JestClientFactory factory = new JestClientFactory();
+		factory.setHttpClientConfig(null);
+		
+		//JestClient client = factory.getObject();
 	}
 }
